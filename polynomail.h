27b297f8746e150e9f5968d2ee5551821407b038
Alec Zabel-Mena <alec.zabel@upr.edu>
2022-03-15 18:30:44 -0400

    Started work on polynomial library. Put gcd.h in .gitignore for now.

--- polynomail.h
+++ polynomail.h
@@ -1,19 +1,60 @@
 #ifndef POLYOMIAL_H
 #define POLYOMIAL_H
+
 #include<vector>
 
-template<class K>
+template<class K, T, A, B, C>
 
 class Poly{
-    template<class T>
     private:
-        unsigned long long deg;
-        T coef[];
+        long long deg;
+        std::vector<K> coef;
     public:
+
+        //Constructors and initializers.
         Poly<K> (){
+            clear();
         }
         Poly<K> (T a){
+            coef.clear();
+            deg = 0;
+            coef.resize(1);
+            coef[0] = {K(a)};
         }
-}
+        Poly<K> (const long long arr_size,  const T *arr){
+            if(arr_size < 0){
+                clear();
+            }
+
+            coef.clear();
+            deg = arr_size;
+            coef.resize(deg+1);
+
+            for(int i = 0; i < deg ; i++){
+                coef[i] = K(arr[i]);
+            }
+        }
+
+        void clear(){
+            deg = -1;
+            coef.resize(1);
+            coef[0] = {K(0)};
+        }
+
+        // Getters
+
+        long long deg(){
+            return deg;
+        }
+
+        K get(long long k){
+            return coef[k];
+        }
+        K operator[](long long k){
+            return coef[k];
+        }
+
+        // Setters.
+};
 
 #endif
